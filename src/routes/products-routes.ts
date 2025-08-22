6274c495117090f1a03e0626d8204e4703d311f2
Edu-Marinho <marinhojcc@hotmail.com>
2025-08-22 17:01:39 -0300

    inicio do projeto

---
+++ src/routes/products-routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { ProductController } from "../controllers/products-controllers";
+
+const productsRoutes = Router();
+const productsController = new ProductController()
+
+productsRoutes.get("/", productsController.index);
+
+export { productsRoutes };
+
